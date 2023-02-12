284dd7ad69ee9b492ea8f4d54768849981c7b96b
YuliiaPaziura <yuliiapaziura@gmail.com>
2023-02-12 16:43:46 +0200

    Add database

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { db } from './database';
+
 import Desk from './components/Desk/Desk';
 
 import './App.css';
@@ -8,13 +10,37 @@ function App() {
       <h1 style={{fontWeight:'bolder'}}> To Do list</h1>
         <div className='bord'>
             <Desk color='#4F646F' title='To Do'>
-
+              {
+                db.todo.map((item, i)=> {
+                  return (
+                    <>
+                      <p>{item.task}</p>
+                    </>
+                  )
+                })
+              }
             </Desk>
             <Desk color='#A37B73' title='In progress'>
-
+              {
+                  db.inProgress.map((item, i)=> {
+                    return (
+                      <>
+                        <p>{item.task}</p>
+                      </>
+                    )
+                  })
+                }
             </Desk>
             <Desk color='#606c38' title='Done'>
-
+              {
+                  db.done.map((item, i)=> {
+                    return (
+                      <>
+                        <p>{item.task}</p>
+                      </>
+                    )
+                  })
+                }
             </Desk>
         </div>
     </div>
---
+++ src/database.js
@@ -0,0 +1,43 @@
+export const db = {
+    'todo': [
+        {
+            'task': 'Code',
+            'descr': 'kjgkjbkjbkljb',
+            'priority' : 'Middle',
+        },
+        {
+            'task': 'Code1',
+            'descr': 'kjgkjbkjbkljb',
+            'priority' : 'Middle',
+        },
+    ],
+    'inProgress': [
+        {
+            'task': 'Code2',
+            'descr': 'kjgkjbkjbkljb',
+            'priority' : 'Middle',
+        },
+        {
+            'task': 'Code3',
+            'descr': 'kjgkjbkjbkljb',
+            'priority' : 'Middle',
+        },
+    ],
+    'done' : [
+        {
+            'task': 'Code4',
+            'descr': 'kjgkjbkjbkljb',
+            'priority' : 'Middle',
+        },
+        {
+            'task': 'Code5',
+            'descr': 'kjgkjbkjbkljb',
+            'priority' : 'Middle',
+        },
+        {
+            'task': 'Code6',
+            'descr': 'kjgkjbkjbkljb',
+            'priority' : 'Middle',
+        },
+    ],
+};

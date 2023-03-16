0641a17d34430587ecea641beb9783fb043e6ec0
YuliiaPaziura <yuliiapaziura@gmail.com>
2023-03-16 14:27:59 +0200

    Edit firebase init

---
+++ src/service/firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://PROJECT_ID.firebaseio.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  measurementId: "G-MEASUREMENT_ID",
+};
+
+// Initialize Firebase 
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);

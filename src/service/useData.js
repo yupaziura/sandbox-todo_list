0b2621ba4006e1277dd07cccdd9e7aafac254980
YuliiaPaziura <yuliiapaziura@gmail.com>
2023-03-21 13:55:35 +0200

    Edit useData

--- src/service/useData.js
+++ src/service/useData.js
@@ -1,20 +1,42 @@
-import {child,get} from "firebase/database";
+import { useState } from "react";
+import {child, get, set} from "firebase/database";
 import { getData } from "./firebase";
 
 export const useData = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
     const fetchData = async () => {
         const newArr = [];
-        await get(child(getData, `/tasks/${localStorage.getItem('userId')}`)).then((snapshot) => {
-          const fetched = snapshot.val();
-          return fetched;
-        }).then(val=>{
-          if(val){
-             for(let value of Object.entries(val)){
-                 newArr.push(value[1]);
+        try {
+          setLoading(true)
+          await get(child(getData, `/tasks/${localStorage.getItem('userId')}`)).then((snapshot) => {
+            const fetched = snapshot.val();
+            return fetched;
+          }).then(val=>{
+            if(val){
+               for(let value of Object.entries(val)){
+                   newArr.push(value[1]);
+              }
             }
-          }
-        }); 
-        return newArr;
+          }); 
+          setLoading(false)
+          return newArr;  
+        }
+        catch (e) {
+          setError(true)
+          console.log(e)
+        }
       };
-      return fetchData;
+
+      const pushData = async (item, guid) => {
+        try {
+          await set(child(getData, `/tasks/${localStorage.getItem('userId')}/${guid}`), item)
+        }
+        catch (e){
+          console.log(e)
+        }
+      };
+
+      return {fetchData, pushData, loading, error, setLoading, setError};
 }
